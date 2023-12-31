e7a2df40f3dafe4fb5f6148fb66bb62890bd495c
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2023-12-31 14:28:35 +0100

    Code inline

--- components/Prose/ProseCodeInlinex.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <li class="!text-amber-500"><slot /></li>
-</template>
---
+++ components/content/ProseCodeInline.vue
@@ -0,0 +1,10 @@
+<template>
+  <code><slot /></code>
+</template>
+
+<style scoped lang="scss">
+code {
+  color: rgb(var(--base-120) / 1);
+  background: rgb(var(--base-20) / 0.5);
+}
+</style>
