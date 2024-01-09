4953ecfbdd1b32054ef75ec66e74cc490d50bb06
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2024-01-09 16:46:21 +0100

    wanted

--- composables/useTheme.ts
+++ composables/useTheme.ts
@@ -1,5 +1,5 @@
 import type { UmbraInput } from "@umbrajs/core";
-import { umbra, rgbStrippedFormat, isDark } from "@umbrajs/core";
+import { umbra, rgbStrippedFormat } from "@umbrajs/core";
 
 const themeInput = {
   foreground: "#ffffff",
@@ -10,38 +10,17 @@ const themeInput = {
 export const useTheme = (scheme?: UmbraInput) => {
   const input = ref<UmbraInput>(scheme || themeInput);
   const stored = useState("umbra", () => input);
-  const theme = umbra(scheme, (outputs) => {
-    console.log("rex: theme set");
+  const theme = umbra(scheme, {
+    formater: rgbStrippedFormat,
+    callback: (outputs) => {
+      console.log("rex: theme set");
+    },
   });
 
   if (scheme) {
     theme.apply({
       formater: rgbStrippedFormat,
     });
-  }
-
-  function setTheme(input: UmbraInput) {
-    return umbra(input, (outputs) => {
-      stored.value = outputs.input;
-      console.log("rex: theme set");
-    }).apply({
-      formater: rgbStrippedFormat,
-    });
-  }
-
-  function inverseTheme(input: UmbraInput) {
-    return umbra(input, (outputs) => {
-      stored.value = outputs.input;
-      console.log("rex: theme inversed");
-    })
-      .inverse()
-      .apply({
-        formater: rgbStrippedFormat,
-      });
-  }
-
-  function isDarkTheme() {
-    return isDark(stored.value);
   }
 
   return theme;
