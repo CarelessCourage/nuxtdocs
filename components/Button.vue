4df2eead1ccec8b1adfb2266a1496e108ab780d8
Samuel M. Bednarz <samuelmorkbednarz@gmail.com>
2024-01-12 21:32:59 +0100

    modifies secondary button

--- components/Button.vue
+++ components/Button.vue
@@ -20,8 +20,8 @@ const props = defineProps({
 
 const iconClass = "p-2";
 const primary =
-  "border bg-accent-900 text-accent-100 hover:bg-accent-700 hover:border-accent-900";
-const secondary = "border";
+  "bg-accent-900 text-accent-100 hover:bg-accent-700 hover:border-accent-900";
+const secondary = "border-accent-900 text-accent-900 hover:bg-accent-100";
 
 const buttonClass = cn(
   "p-2 border border-transparent px-4 flex gap-2 justify-center items-center rounded-lg duration-100 transition active:bg-base-600 hover:bg-base-300 hover:border-border",
